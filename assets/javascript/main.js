6e1c2de6c5bb5c90f2b81a8ed2130c3c199c9fcb
Justin <justin-girot@hotmail.fr>
2018-12-10 10:29:26 +0100

    externalisation des scripts

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+/**
+ * @name main.js
+ * @desc point d'entrée principal dans l'application javascript
+ */
+
+
+let title = document.getElementById('main-title');
+title.innerHTML = 'hello javascript';
